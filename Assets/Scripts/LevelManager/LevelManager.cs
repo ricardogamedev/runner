e07ccd502df5056e4a7cd1fbab5c911ddb85d5ae
Ricardo <rmachadobdev@gmail.com>
2023-04-11 19:08:52 -0300

    new animations implemenmted

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Ebac.Core.Singleton;
+using DG.Tweening;
 
 
 public class LevelManager : MonoBehaviour
@@ -20,7 +21,12 @@ public class LevelManager : MonoBehaviour
     private List<LevelPieceBase> _spawnedPieces = new List<LevelPieceBase>();
     private LevelPieceBaseSetup _currentSetup;
 
-    private void Awake()
+    [Header("Animation")]
+    public float scaleDuration = .2f;
+    public float scaleTimeBetweenPieces = .1f;
+    public Ease ease = Ease.OutBack;
+
+    private void Start()
     {
         // SpawnNextLevel();
         CreateLevelPieces();
@@ -83,6 +89,28 @@ public class LevelManager : MonoBehaviour
         }
 
         ColorManager.Instance.ChangeColorByType(_currentSetup.artType);
+
+        StartCoroutine(ScalePiecesByTime());
+        
+    }
+
+    IEnumerator ScalePiecesByTime()
+    {
+        foreach(var p in _spawnedPieces)
+        {
+            p.transform.localScale = Vector3.zero;
+        }
+
+        yield return null;
+        
+        for(int i = 0; i<_spawnedPieces.Count; i++)
+        {
+            _spawnedPieces[i].transform.DOScale(1, scaleDuration).SetEase(ease);
+
+            yield return new WaitForSeconds(scaleTimeBetweenPieces);
+        }
+
+        CoinsAnimationManager.Instance.StartAnimations();
     }
 
     private void CreateLevelPiece(List<LevelPieceBase> list)
